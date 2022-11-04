fe708020153562870412e95cc48cde1dd6ff0b47
bnyte <bnytezz@gmail.com>
2022-11-04 15:18:51 +0800

    init project

---
+++ doraemon-spring-boot-starter/src/main/java/org/kushinae/doraemon/autoconfigure/auto/DoraemonAutoConfiguration.java
@@ -0,0 +1,13 @@
+package org.kushinae.doraemon.autoconfigure.auto;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author bnyte
+ * @since 1.0.0
+ */
+@Configuration
+public class DoraemonAutoConfiguration {
+
+
+}
